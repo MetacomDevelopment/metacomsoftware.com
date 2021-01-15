725a5fb035785b0e440377ad5e8d7597044cd011
John Grattan <contact@johngrattan.com>
2021-01-15 17:20:03 -0500

    Using gatsby-node.esm.js instead for ES6 support

---
+++ gatsby-site/gatsby-node.esm.js
@@ -0,0 +1,36 @@
+import path from 'path';
+
+async function turnPostsIntoPages({ graphql, actions }) {
+  // 1. Get template for posts
+  const postTemplate = path.resolve('./src/templates/Post.jsx');
+  // 2. Query all posts
+  const { data } = await graphql(`
+    query {
+      posts: allSanityPost(filter: { slug: { current: { ne: "null" } } }) {
+        nodes {
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
+  // 3. Loop over each post and create a page for them
+  data.posts.nodes.forEach((post) => {
+    actions.createPage({
+      path: `blog/${post.slug.current}`,
+      component: postTemplate,
+      context: { slug: post.slug.current },
+    });
+  });
+}
+
+export async function createPages(params) {
+  // Create pages dynamically
+  // 1. Posts
+  await turnPostsIntoPages(params);
+  // 2. Services
+  // 3. Service Areas
+}
