59cb1cd69122a7ca90c99bb6818d4ebad6a952c6
John Grattan <contact@johngrattan.com>
2021-02-05 17:56:25 -0500

    Turned off sanity watchmode

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
         token: process.env.SANITY_TOKEN,
-        watchMode: true,
+        watchMode: false,
         overlayDrafts: true,
       },
     },
