9d2a24fad605235e1fcdbc47646532de999a1446
John Grattan <contact@johngrattan.com>
2021-08-28 14:18:54 -0400

    Updated Sanity/Netlify API

--- studio/parts/dashboardConfig.js
+++ studio/parts/dashboardConfig.js
@@ -7,7 +7,7 @@ export default {
         placeholder: 'Make a note so you remember!',
         backgroundColor: '#242a2f',
         color: '#efeff2',
-      }
+      },
     },
     {
       name: 'netlify',
@@ -16,15 +16,17 @@ export default {
         sites: [
           {
             title: 'Sanity Studio',
-            apiId: '7c5d94c7-428b-400e-873f-f3b48a08459c',
-            buildHookId: 'https://api.netlify.com/build_hooks/60b0434d351fbb00e50e2268',
-            name: 'braven-landscape',
+            apiId: 'f5d17d45-d898-42eb-b8ca-97c37147948a',
+            buildHookId:
+              'https://api.netlify.com/build_hooks/60b1a295f776af78ee4454ed',
+            name: 'allstarconnections',
           },
           {
             title: 'Website',
-            apiId: '7c5d94c7-428b-400e-873f-f3b48a08459c',
-            buildHookId: 'https://api.netlify.com/build_hooks/60b0435697acf700ccb06385',
-            name: 'braven-landscape',
+            apiId: 'f5d17d45-d898-42eb-b8ca-97c37147948a',
+            buildHookId:
+              'https://api.netlify.com/build_hooks/60b1a29da84d5e7f4927d296',
+            name: 'allstarconnections',
           },
         ],
       },
